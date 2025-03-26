9f6f9e7e95528ba0c707e62cdc2364ff479b9e78
isaac451-andrade <166961938+isaac451-andrade@users.noreply.github.com>
2025-03-26 15:16:48 -0300

    Deploy 2

--- detector-de-fake-news-api/venv/Scripts/activate.bat
+++ detector-de-fake-news-api/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\projetosETestes\cursojavascript\nicoBot\projeto-detector-de-fake-news\detector-de-fake-news-api\venv
+set VIRTUAL_ENV=C:\projetosETestes\cursojavascript\CopiaNicoBot\projeto-detector-de-fake-news\detector-de-fake-news-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
